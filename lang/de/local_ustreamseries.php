95497d3b1ac0ab72b73084ed1e9b87b2d32dd05b
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2022-02-02 13:59:27 +0100

    german translation

--- lang/de/local_ustreamseries.php
+++ lang/de/local_ustreamseries.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'local_contactlist', language 'en'.
+ * Strings for component 'local_contactlist', language 'de'.
  *
  * @package       local_ustreamseries
  * @author        Thomas Wedekind
@@ -24,4 +24,27 @@
  * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'u:stream series connector';
+$string['link_ustream'] = 'Verknüpfe u:stream Serien';
+$string['link_stream_form_create'] = 'Neue Serie erstellen';
+$string['link_stream_form_create_lv'] = 'Neue LV-Serie erstellen';
+$string['link_stream_form_link'] = 'LV-Serie verknüpfen';
+$string['link_stream_form_link_other'] = 'Andere Serien verknüpfen';
+$string['link_stream_form_select_action'] = 'Aktion auswählen';
+$string['link_stream_form_seriesname'] = 'u:stream Serienname';
+$string['link_stream_form_link_all_course_series'] = 'Alle Kursserien verknüpfen';
+$string['link_stream_form_series_id_select'] = 'Serie auswählen';
+$string['link_stream_form_series_id'] = 'Serien-ID';
+$string['link_stream_form_seriesnotexistsorconnected'] = 'Diese Serie existiert nicht (mehr) oder ist bereits mit diesem Kurs verknüpft';
+$string['link_stream_settingsmenu'] = 'Neue u:stream-Serien einbinden';
+$string['navigationname'] = 'u:stream';
+$string['pluginname'] = 'u:stream Serien';
+$string['series_editable'] = 'WARNUNG! Alle hier eingebundenen Serien sind von ALLEN Kursleitern über Moodle editierbar, zusätzlich zur eingestellten Nutzerschaft in u:stream. Verlinken Sie nur u:stream Serien in Kursen, wo Sie ihren Leitern vertrauen. Wir arbeiten mit Hochdruck an einer passenderen Lösung. Vielen Dank für Ihre Geduld.';
+$string['series_creation_success'] = 'Serie erfolgreich erstellt';
+$string['series_creation_failed'] = 'Fehler beim Erstellen der Serie. Bitte kontaktieren sie den Moodle-Support.';
+$string['series_link_failed'] = 'Fehler beim Verknüpfen der Serie. Bitte kontaktieren Sie den Moodle-Support.';
+$string['series_link_success'] = 'Serie erfolgreich verknüpft.';
+$string['ustream'] = 'u:stream';
+$string['ustreamseries:create'] = 'Neue u:stream Serien erstellen';
+$string['ustreamseries:link'] = 'LV-Stream aus u:stream verknüpfen';
+$string['ustreamseries:link_other'] = 'Beliebige u:stream-Serien verknüpfen';
+$string['warning_noustream'] = 'Bisher ist keine u:stream-Serie mit diesem Kurs verknüpft. Sie können neue u:stream-Serien im Kursmenü hinzufügen, oder indem sie <a href={$a}>hier</a> klicken.';
