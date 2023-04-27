0ae427ebf990bd29ed41f65af51ef95b184a06a8
Adrian Czermak <adrian.czermak@univie.ac.at>
2023-04-27 10:37:35 +0200

    added nav node

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023022100;
+$plugin->version = 2023022101;
 $plugin->requires = 2020110900;
 $plugin->component = 'local_ustreamseries';
 $plugin->maturity = MATURITY_STABLE;
